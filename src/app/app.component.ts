ad12cba07224f804fa41f140081e31b56b690446
Emmanuel Evuazeze <eevuazeze@byteworks.com.ng>
2020-03-27 10:09:03 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <router-outlet></router-outlet>
-  `,
-  styleUrls: ['./app.component.css']
+  `
 })
 export class AppComponent {
 }
